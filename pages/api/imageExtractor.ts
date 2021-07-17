06cfcedbe07f0d977a3834e6836f2bd161e06df2
Chiro <sachinsinghroxx@gmail.com>
2021-07-17 13:05:40 +0530

    finished image url refresh

--- pages/api/imageExtractor.ts
+++ pages/api/imageExtractor.ts
@@ -1,14 +1,19 @@
 // import next api respon and request
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function imageExtractor(
+export default async function imageExtractor(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   // retrive random image from the internet
 
-  const url = "https://picsum.photos/200/300/?random";
+  // generate a random number between 0 and 255
+  const randomNumber = Math.floor(Math.random() * 256);
+
+//   const url = await fetch(
+//     "https://picsum.photos/200/200/?random=" + randomNumber
+//   ).then((response) => response);
 
   // return the image as a canvas
-  res.status(200).json({ image: url });
+  res.status(200).json({ image: "https://picsum.photos/200/200/?random=" + randomNumber });
 }
